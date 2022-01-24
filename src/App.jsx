aaa6bd2d82d4085e3f068e2c8a6cbf7e2c10da23
Dakouri M. Kobri <maurille.kobri@gmail.com>
2022-01-24 21:35:06 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import firebase from './FirebaseConfig';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Meal Recipes</h1>
+      </div>
+    </div>
+  );
+}
